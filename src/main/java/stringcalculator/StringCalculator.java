fcefe8d49b903550564ce79e3682adcd0db7f9ca
Martin <martin@seventy2eleven.de>
2013-10-16 08:19:52 +0200

    Added tests and updated flow.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,7 +4,18 @@ import org.apache.commons.lang.StringUtils;
 
 public class StringCalculator {
 
-	public Integer add(String string) {
-		return StringUtils.isBlank(string)?0:1;
+	public Integer add(String numbers) {
+		if (StringUtils.isEmpty(numbers)) {
+			return 0;
+		}
+		return splitAndAddNumbers(numbers);
+	}
+
+	private Integer splitAndAddNumbers(String numbers) {
+		int sum = 0;
+		for (String number : StringUtils.split(numbers, ',')) {
+			sum += Integer.parseInt(number);
+		}
+		return sum;
 	}
 }
--- src/test/java/stringcalculator/tests/StringCalculatorTests.java
+++ src/test/java/stringcalculator/tests/StringCalculatorTests.java
@@ -17,12 +17,54 @@ public class StringCalculatorTests {
 	}
 
 	@Test
-	public void foo() {
+	public void whenAddingEmptyStringAddShouldReturnZero() {
 		assertEquals(Integer.valueOf(0), calc.add(""));
 	}
 
 	@Test
-	public void bar() {
+	public void whenAddingOneAddShouldReturnOne() {
 		assertEquals(Integer.valueOf(1), calc.add("1"));
+	}
+
+	@Test
+	public void whenAddingZeroAddShouldReturnZero() {
+		assertEquals(Integer.valueOf(0), calc.add("0"));
+	}
+
+	@Test
+	public void whenAddingTwoToThreeAddShouldReturnFive() {
+		assertEquals(Integer.valueOf(5), calc.add("2,3"));
+	}
+
+	@Test
+	public void whenAddingSeveralNumbersItShouldReturnTheSum() {
+		assertEquals(Integer.valueOf(15), calc.add(generateNumbers(1,15)));
+	}
+
+	@Test
+	public void whenAdding20Time153ItShouldReturn3060() {
+		assertEquals(Integer.valueOf(3060), calc.add(generateNumbers(20,153)));
+	}
+	
+	@Test
+	public void whenAdding12345678Times9999tShouldReturn3060() {
+		assertEquals(Integer.valueOf(12345678*9999), calc.add(generateNumbers(12345678,9999)));
+	}
+	
+	@Test
+	public void testGenerateNumbersHelperMethod() {
+		String numbers = generateNumbers(3, 15);
+		assertEquals("3,3,3,3,3,3,3,3,3,3,3,3,3,3,3", numbers);
+	}
+
+	private String generateNumbers(int number, int repetitions) {
+		StringBuilder sb = new StringBuilder();
+		String delim = "";
+		for (int currentNumber = 0; currentNumber < repetitions; currentNumber++) {
+			sb.append(delim);
+			sb.append(number);
+			delim = ",";
+		}
+		return sb.toString();
 	}
 }
