0b60692480e8d555a315c49fd273e9bff78b24f0
Martin <martin@seventy2eleven.de>
2013-10-18 19:41:54 +0200

    Added tests and hit a sudden end.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -8,14 +8,25 @@ public class StringCalculator {
 		if (StringUtils.isEmpty(numbers)) {
 			return 0;
 		}
-		return splitAndAddNumbers(numbers);
+		return splitAndAddNumbers(removeNewLines(numbers));
+	}
+
+	private String removeNewLines(String numbers) {
+		return numbers.replace("\n", ",");
 	}
 
 	private Integer splitAndAddNumbers(String numbers) {
 		int sum = 0;
 		for (String number : StringUtils.split(numbers, ',')) {
-			sum += Integer.parseInt(number);
+			sum += parseNumber(number);
 		}
 		return sum;
+	}
+
+	private int parseNumber(String number) {
+		if (number.trim().length() == 0) {
+			return 0;
+		}
+		return Integer.parseInt(number.trim());
 	}
 }
--- src/test/java/stringcalculator/tests/StringCalculatorTests.java
+++ src/test/java/stringcalculator/tests/StringCalculatorTests.java
@@ -2,6 +2,7 @@ package stringcalculator.tests;
 
 import static org.junit.Assert.assertEquals;
 
+import org.codehaus.plexus.util.StringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -38,17 +39,63 @@ public class StringCalculatorTests {
 
 	@Test
 	public void whenAddingSeveralNumbersItShouldReturnTheSum() {
-		assertEquals(Integer.valueOf(15), calc.add(generateNumbers(1,15)));
+		assertEquals(Integer.valueOf(15), calc.add(generateNumbers(1, 15)));
 	}
 
 	@Test
 	public void whenAdding20Time153ItShouldReturn3060() {
-		assertEquals(Integer.valueOf(3060), calc.add(generateNumbers(20,153)));
+		assertEquals(Integer.valueOf(3060), calc.add(generateNumbers(20, 153)));
+	}
+
+	@Test
+	public void whenAdding12345678Times9999tShouldReturn3060() {
+		assertEquals(Integer.valueOf(12345678 * 9999),
+				calc.add(generateNumbers(12345678, 9999)));
+	}
+
+	@Test
+	public void whenAddingAStringWithOneNewLineItShouldBeCalculated() {
+		assertEquals(Integer.valueOf(27), calc.add("14\n13"));
+	}
+
+	@Test
+	public void whenAddingAStringWithOneNewLineAtTheEndItShouldBeCalculated() {
+		assertEquals(Integer.valueOf(27), calc.add("14,13\n"));
+	}
+
+	@Test
+	public void whenAddingAStringWithOneNewLineAtTheBeginningItShouldBeCalculated() {
+		assertEquals(Integer.valueOf(27), calc.add("\n14,13"));
+	}
+
+	@Test
+	public void whenAddingAStringWithSeveralLinesAtItShouldBeCalculated() {
+		assertEquals(Integer.valueOf(27), calc.add("8\n6\n4\n9"));
+	}
+
+	@Test
+	public void whenAddingSomeStringWithSpacesItShouldBeCalculated(){
+		assertEquals(Integer.valueOf(12), calc.add("8, 4"));
 	}
 	
 	@Test
-	public void whenAdding12345678Times9999tShouldReturn3060() {
-		assertEquals(Integer.valueOf(12345678*9999), calc.add(generateNumbers(12345678,9999)));
+	public void whenAddingSomeStringWithSpacesAndANewlineAfterwardsItShouldBeCalculated(){
+		assertEquals(Integer.valueOf(7), calc.add("3, \n4"));
+	}
+	
+	@Test
+	public void whenAddingSomeStringWithSeveralNewlinesBeforeANumberItShouldBeCalculated(){
+		assertEquals(Integer.valueOf(10),calc.add("\n\n\n   \n \n\n1\n\n2\n \n\n3,4"));
+	}
+	
+	@Test
+	public void whenANewLineInsteadOfACommaItShouldCalculate(){
+		assertEquals(Integer.valueOf(6),calc.add("1\n2,3"));
+	}
+	
+	@Test
+	public void whenANewlineOrACommaIsAtTheEndItShouldNotWork(){
+		assertEquals(Integer.valueOf(1),calc.add("1,\n"));
 	}
 	
 	@Test
